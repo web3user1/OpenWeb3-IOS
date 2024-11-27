e9a8a046385dc54199d36148e4c3b75e52eadfae
biu <biu@mtsocialdao.com>
2024-11-27 15:49:45 +0800

    1.0.3

--- Sample2/Sample2/MiniSDKManager.swift
+++ Sample2/Sample2/MiniSDKManager.swift
@@ -8,13 +8,31 @@
 import UIKit
 import OpenWeb3Lib
 
+enum AppType : String {
+    case MINIAPP
+    case WEBPAGE
+}
+
 @objcMembers
 class MiniSDKManager: NSObject, IAppDelegate {
+    
+    let SHORTCUT_LINK = "openweb3_link"
+    let SHORTCUT_TYPE = "openweb3_type"
+    
+    let APP_ID_WALLET = "9"
+    let APP_ID_MARKETPLACE = "10"
+    let APP_ID_APPMASTER = "2lv8dp7JjF2AU0iEk2rMYUaySjU"
+
     static let shared = MiniSDKManager()
     
     var openPlatformPlugin: OpenPlatformPlugin
     
     var miniAppService: MiniAppService
+    
+    var isAuth: Bool = false
+    
+    private var shortcutItem: UIApplicationShortcutItem? = nil
+    
     
     let idToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5ZTg2YmMxYjBjMjI5NDBkNDRjZjNmNWI4NWZmZWYwYTZjNmQzNzIiLCJ0eXAiOiJKV1QifQ.eyJ1aWQiOiJtdHNvY2lhbEBnbWFpLmNvbSIsImlzcyI6ImZpcmViYXNlLWFkbWluc2RrLTR2ejMwQG10c29jaWFsLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwiZXhwIjoxNzUxODEzMTUxLCJpYXQiOjE3MjA3MDkxNTEsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHl0b29sa2l0Lmdvb2dsZWFwaXMuY29tL2dvb2dsZS5pZGVudGl0eS5pZGVudGl0eXRvb2xraXQudjEuSWRlbnRpdHlUb29sa2l0Iiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstNHZ6MzBAbXRzb2NpYWwuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20ifQ.IxKdRdKZhbiYIbRd7nzieti--cEHNA-65rg01Wl6h64cXVviPlZ5MsaueN4uRUODtYs6mdYMAteoy54Wi0GMJzIGMkClUJtbWTOfW1L43YdB4R4XhhVsx2gvF8iCF0MQrDB8ekfyWEqbBdJdbM0BUH0NjSl1Mg15Ta-Rx1cYsk41vmDULpkqHJl93Xjuu2ts1KY7Rs3kNp1NAj9-gC4kHzUG57dmvLqteb4qMmUN7h2tq_np3rdGUBRzxB_YBOnqICAmJ-u6knV_XT08Ep1fB-H_HqR41W_FMgv3EW-V5pApDddJttNjaTy8rJfy2xL9mOhQV0OH-1vHjm4Mz2Jpeg"
     
@@ -32,12 +50,26 @@ class MiniSDKManager: NSObject, IAppDelegate {
         }
     }
     
+    private func preloadApp() {
+        [APP_ID_MARKETPLACE,APP_ID_WALLET,APP_ID_APPMASTER].forEach { id in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                let config = WebAppPreloadParameters.Builder()
+                    .miniAppId(id)
+                    .build()
+                self?.miniAppService.preload(config: config)
+            }
+        }
+    }
+    
     func signIn() {
         self.openPlatformPlugin.signIn(
             verifier: "123",
             idTokenProvider: tokenProvider,
-            onVerifierSuccess: {
+            onVerifierSuccess: { [weak self] in
                 // Do something
+                self?.isAuth = true
+                self?.preloadApp()
+                self?.progressOnAuth()
             },
             onVerifierFailure: { code, message in
                 print("Verifier Err, code= \(code), message: \(String(describing: message))")
@@ -59,30 +91,96 @@ class MiniSDKManager: NSObject, IAppDelegate {
                 .floatWindowSize(width: 90.0, height: 159.0)
                 .build()
             
-            MiniSDKManager.shared.miniAppService.setup(config: appConfig)
+           miniAppService.setup(config: appConfig)
         }
     }
     
-    func launchWithUrl(vc: UIViewController, url: String) {
+    private func progressOnAuth() {
+        if let shortcutItem = self.shortcutItem {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                let _ = self.handleShortcutItem(shortcutItem)
+                self.shortcutItem = nil
+            }
+        }
+    }
+    
+    func handleShortcutItem(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        
+        guard let userInfo = shortcutItem.userInfo as? [String: String] else {
+            return false
+        }
+        
+        if !isAuth {
+            self.shortcutItem = shortcutItem
+            return true
+        }
+        
+        if let type = userInfo[SHORTCUT_TYPE], let link = userInfo[SHORTCUT_LINK] {
+            navigateToScreen(link: link, type: type)
+            return true
+        }
+        
+        return false
+    }
+    
+    func getTopViewController() -> UIViewController? {
+        guard let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+              let rootViewController = windowScene.windows.first?.rootViewController else {
+            print("No active window or rootViewController found")
+            return nil
+        }
+    
+        var topViewController = rootViewController
+        while let presentedVC = topViewController.presentedViewController {
+            topViewController = presentedVC
+        }
+        
+        return topViewController
+    }
+    
+    func navigateToScreen(link: String, type: String) {
+        if let vc = getTopViewController() {
+            let _ = self.launchWithUrl(vc: vc, url: link, type: type)
+        }
+    }
+    
+    func launch(config: WebAppLaunchParameters) -> IMiniApp? {
+        return miniAppService.launch(config: config)
+    }
+    
+    func launchWithUrl(vc: UIViewController, url: String, type: String = AppType.MINIAPP.rawValue)  {
         
         if false == url.starts(with: "https://") {
             return
         }
         
-        do{
-            let config = try WebAppLaunchWithDialogParameters.Builder()
-                .parentVC(vc)
-                .url(url)
-                .isLaunchUrl(true)
-                .build()
-            
-             miniAppService.launch(config: config)
-            
+        if type == AppType.MINIAPP.rawValue {
+            do{
+                let config = try WebAppLaunchWithDialogParameters.Builder()
+                    .parentVC(vc)
+                    .url(url)
+                    .build()
+                
+              let _ = miniAppService.launch(config: config)
+                
+            }
+            catch {
+                print("An error occurred: \(error)")
+            }
+        } else {
+            do{
+                let config = try DAppLaunchParameters.Builder()
+                    .parentVC(vc)
+                    .url(url)
+                    .build()
+                
+                let _ = miniAppService.launch(config: config)
+                
+            }
+            catch {
+                print("An error occurred: \(error)")
+            }
         }
-        catch {
-            print("An error occurred: \(error)")
-        }
-
     }
     
     var customMethodProvider: (IMiniApp, String, String?, @escaping (String?) -> Void )-> Bool = { _, method, params, callback in
@@ -181,22 +279,47 @@ class MiniSDKManager: NSObject, IAppDelegate {
     }
     
     /**
-     * 分享App
-     */
-    func shareWebApp(app: IMiniApp) {
-    }
-    
-    /**
      * Api Error
      */
     func onApiError(error: ApiError) {
         switch(error) {
+        case .authInvalid:
+            signIn()
         case .requestFailed(let code, let message):
             if code == 401 {
                 signIn()
             }
         default:
             break
+        }
+    }
+    
+    func onMinimization(app: any OpenWeb3Lib.IMiniApp) {
+        
+    }
+    
+    func onMaximize(app: any OpenWeb3Lib.IMiniApp) {
+        
+    }
+    
+    func onMoreButtonClick(app: any OpenWeb3Lib.IMiniApp, menus: [String]) -> Bool {
+        return false
+    }
+    
+    func onClickMenu(app: any OpenWeb3Lib.IMiniApp, type: String) {
+        if type == "FEEDBACK" {
+            if let miniAppVc = app.getVC() {
+                let chatViewController = ChatViewController()
+                miniAppVc.present(chatViewController, animated: true)
+            }
+        }
+        else if type == "SHARE" {
+            Task {
+                let shareInfo = await app.getShareInfo()
+                guard let title = shareInfo?.title else {
+                    return
+                }
+            }
         }
     }
 
